551127b98147545d6875aac8343dfe30a6b522cb
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2025-03-19 18:39:23 +0530

    Get current logged-in sender details to new request form.

--- backend/controllers/authController.js
+++ backend/controllers/authController.js
@@ -47,3 +47,16 @@ export const login = async (req, res) => {
         res.status(400).json({ error: error.message });
     }
 };
+
+
+// Get current logged-in user details
+export const getSenderDetails = async (req, res) => {
+    try {
+        const user = await User.findById(req.user.id).select("-password");
+        if (!user) return res.status(404).json({ error: "User not found" });
+
+        res.status(200).json(user);
+    } catch (error) {
+        res.status(500).json({ error: error.message });
+    }
+};
