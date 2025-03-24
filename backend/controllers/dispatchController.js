3130e1ca404b1224cee27769e8b8185fea26053e
gimhaninavodya <146187436+gimhaninavodya@users.noreply.github.com>
2025-03-24 18:13:10 +0530

    Add dispatch approve and reject section and change table format

--- backend/controllers/dispatchController.js
+++ backend/controllers/dispatchController.js
@@ -25,3 +25,34 @@ export const getDispatchById = async(req, res) => {
     res.status(500).json({ message: 'Error fetching request', error: err });
   }
 };
+
+// update dispatch status
+export const updateDispatchStatus = async (req, res) => {
+  const { id } = req.params;
+  const { dispatchStatus, approverName, serviceNo, comment } = req.body;
+
+  // Validation
+  if (!approverName || !serviceNo) {
+    return res.status(400).json({ message: "Name and Service Number are required!" });
+  }
+  if (dispatchStatus === "Rejected" && !comment) {
+    return res.status(400).json({ message: "Comment is required for rejection!" });
+  }
+
+  try {
+    const request = await Request.findById(id);
+    if (!request) {
+      return res.status(404).json({ message: "Request not found" });
+    }
+
+    request.dispatchStatus = dispatchStatus;
+    request.approverName = approverName;
+    request.serviceNo = serviceNo;
+    request.comment = comment || ""; 
+
+    await request.save();
+    res.status(200).json({ message: `Request ${dispatchStatus} successfully!` });
+  } catch (error) {
+    res.status(500).json({ message: "Error updating approval status", error });
+  }
+};
