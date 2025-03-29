05e9805ef7046b145bfb0a9fdb6b04c38ca03a06
shainiRose <shainiu22@gmail.com>
2025-03-29 08:34:34 +0530

    add missing fields for the request

---
+++ backend/models/requestModel.js
@@ -0,0 +1,56 @@
+import mongoose from 'mongoose';
+
+const itemSchema = new mongoose.Schema({
+  itemName: { type: String, required: true },
+  serialNo: { type: String, required: true },
+  category: { type: String, required: true },
+  quantity: { type: Number, required: true },
+  description: { type: String, required: true },
+  returnable: { type: String, required: true },
+  image: { type: String }
+});
+
+const requestSchema = new mongoose.Schema({
+  items: [itemSchema], // Array of items
+  outLocation: { type: String, required: true },
+  inLocation: { type: String, required: true },
+  executiveOfficer: { type: String, required: true },
+  receiverName: { type: String, required: true },
+  receiverContact: { type: Number, required: true },
+  receiverGroup: { type: String, required: true },
+  receiverServiceNumber: { type: Number },
+  vehicleNumber: { type: Number },
+  byHand: { type: String },
+  status: {
+    type: String,
+    enum: ['Pending', 'Approved', 'Rejected'],
+    default: 'Pending'
+  },
+  verify: {
+    type: String,
+    enum: ['Pending', 'Verified', 'Rejected'],
+    default: 'Pending'
+  },
+  dispatchStatusOut: {
+    type: String,
+    enum: ["Pending", "Approved", "Rejected"],
+    default: "Pending",
+  },
+  dispatchStatusIn: {
+    type: String,
+    enum: ["Pending", "Approved", "Rejected"],
+    default: "Pending",
+  },
+  approverNameOut: { type: String },
+  serviceNoOut: { type: String },
+  commentOut: { type: String },
+  approverNameIn: {  type: String },
+  serviceNoIn: {  type: String },
+  commentIn: { type: String }
+}, {  // <-- The timestamps option should be part of the Schema options object
+  timestamps: true,
+});
+
+const Request = mongoose.model('Request', requestSchema);
+
+export default Request;
